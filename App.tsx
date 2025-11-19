fe27316e8a97ac187a62084f1cf77199e435ec37
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-19 18:09:23 +0500

    ADD: Стилизация

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
+const ourStyle = {
+    textStyle: { color: 'blue' },
+}
+const textStyle = { color: 'magenta' }
+
 export default function App() {
     return (
         <View style={styles.container}>
             <View>
-                <Text>REACT NATIVE</Text>
+                <Text style={{ color: 'white', backgroundColor: 'blue', padding: 10 }}>
+                    REACT NATIVE
+                </Text>
             </View>
-            <Text>Моё первое приложение</Text>
+            <Text style={styles.textStyle}>Моё первое приложение</Text>\
+            <Text style={ourStyle.textStyle}>EXPO</Text>
             <StatusBar style="auto" />
-            <TextInput value="привет" />
+            <TextInput value="привет" style={textStyle} />
             <Button title="Я кнопка" />
         </View>
     )
@@ -21,5 +29,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    textStyle: {
+        color: 'green',
+        fontSize: 24,
+        borderWidth: 1,
+        borderColor: 'red',
+        padding: 10,
+        margin: 10,
     },
 })
