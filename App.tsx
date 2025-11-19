3191d4887d9313c769523ad3f61934b6140a8803
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-19 18:26:10 +0500

    ADD: Основы flex

--- App.tsx
+++ App.tsx
@@ -9,33 +9,25 @@ const textStyle = { color: 'magenta' }
 export default function App() {
     return (
         <View style={styles.container}>
-            <View>
-                <Text style={{ color: 'white', backgroundColor: 'blue', padding: 10 }}>
-                    REACT NATIVE
-                </Text>
+            <View style={styles.top}>
+                <Text style={styles.textStyle}>Моё первое приложение</Text>
+                <Button title="Я кнопка" />
             </View>
-            <Text style={styles.textStyle}>Моё первое приложение</Text>\
-            <Text style={ourStyle.textStyle}>EXPO</Text>
-            <StatusBar style="auto" />
-            <TextInput value="привет" style={textStyle} />
-            <Button title="Я кнопка" />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: 70,
     },
     textStyle: {
         color: 'green',
         fontSize: 24,
         borderWidth: 1,
         borderColor: 'red',
-        padding: 10,
-        margin: 10,
+    },
+    top: {
+        flexDirection: 'row',
     },
 })
