893f5d78db02a222544e0309a64294f00957b0cc
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-22 15:09:23 +0500

    ADD: Основы анимации

--- shared/Button/Button.tsx
+++ shared/Button/Button.tsx
@@ -1,12 +1,28 @@
-import { Pressable, PressableProps, StyleSheet, Text, View } from 'react-native'
+import { Animated, Pressable, PressableProps, StyleSheet, Text, View } from 'react-native'
 import { Colors, Fonts, Radius } from '../tokens'
 
 export const Button = ({ text, ...props }: PressableProps & { text: string }) => {
+    const animatedValue = new Animated.ValueXY({ x: 0, y: 0 })
+    Animated.timing(animatedValue, {
+        toValue: {
+            x: 100,
+            y: 100,
+        },
+        duration: 2000,
+        useNativeDriver: true,
+    }).start()
     return (
         <Pressable {...props} onPress={() => console.log('press')}>
-            <View style={styles.button}>
+            <Animated.View
+                style={{
+                    ...styles.button,
+                    transform: [
+                        { translateX: animatedValue.x },
+                        { translateY: animatedValue.y },
+                    ],
+                }}>
                 <Text style={styles.buttonText}>{text}</Text>
-            </View>
+            </Animated.View>
         </Pressable>
     )
 }
