a59441f8e52e3b7682359a94c91422beb2ff3870
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-24 12:29:48 +0500

    ADD: Layout

---
+++ app/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from 'expo-router'
+
+export default function RootLayout() {
+    return <Stack />
+}
+
+// import { Slot } from 'expo-router'
+// import { Text } from 'react-native'
+
+// export default function RootLayout() {
+//     return (
+//         <>
+//             <Text>Header</Text>
+//             <Slot />
+//             <Text>Footer</Text>
+//         </>
+//     )
+// }
