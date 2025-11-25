b1f04cc057417bcb0a7e3e02f32410e7ad489741
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-25 22:10:28 +0500

    ADD: Запросы в State

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -3,29 +3,22 @@ import { Colors } from '../../shared/tokens'
 import { CustomLink } from '../../shared/CustomLink/CustomLink'
 import { profileAtom } from '../../entities/user/model/user.state'
 import { useAtom } from 'jotai'
-import { API } from '../../entities/auth/api/api'
+import { loginAtom } from '../../entities/auth/model/auth.state'
 import { useEffect } from 'react'
-import axios from 'axios'
-import { IAuthResponse } from '../../entities/auth/model/auth.interfaces'
 
 export default function AppLayout() {
     const [profile] = useAtom(profileAtom)
-
-    const login = async () => {
-        const { data } = await axios.post<IAuthResponse>(API.login, {
-            email: 'a.b.uvarov@gmail.com',
-            password: '12345678',
-        })
-        console.log(data)
-    }
+    const [auth, login] = useAtom(loginAtom)
 
     useEffect(() => {
-        login()
+        login({ email: 'a.b.uvarov@gmail.com', password: '12345678' })
     }, [])
 
     return (
         <View>
-            <Text style={{ color: Colors.primary }}>{profile.profile?.name} Мои курсы</Text>
+            <Text style={{ color: Colors.primary }}>
+                {profile.profile?.name} Мои курсы {auth?.accessToken}
+            </Text>
             <CustomLink href={'/login'} text={'Логин'} />
             <CustomLink href={'/restore'} text={'Восстановить пароль'} />
             <CustomLink href={'/course/typescript'} text={'Курс ts'} />
--- entities/auth/model/auth.state.ts
+++ entities/auth/model/auth.state.ts
@@ -1,5 +1,9 @@
 import { atomWithStorage, createJSONStorage } from 'jotai/utils'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { IAuthResponse, ILoginRequest } from './auth.interfaces'
+import axios, { AxiosError } from 'axios'
+import { API } from '../api/api'
+import { atom } from 'jotai'
 
 const storage = createJSONStorage<AuthState>(() => AsyncStorage)
 
@@ -11,6 +15,28 @@ export const authAtom = atomWithStorage<AuthState>(
         error: null,
     },
     storage,
+)
+
+export const loginAtom = atom(
+    (get) => get(authAtom),
+    async (_get, set, { email, password }: ILoginRequest) => {
+        set(authAtom, { isLoading: true, accessToken: null, error: null })
+        try {
+            const { data } = await axios.post<IAuthResponse>(API.login, {
+                email,
+                password,
+            })
+            set(authAtom, { isLoading: false, accessToken: data.accessToken, error: null })
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                set(authAtom, {
+                    isLoading: false,
+                    accessToken: null,
+                    error: error.response?.data.message,
+                })
+            }
+        }
+    },
 )
 
 export interface AuthState {
