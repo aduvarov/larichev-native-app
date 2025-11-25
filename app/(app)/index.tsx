5331f5bf41dd76ba5e551745b145d7c66d9dcc12
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-25 13:54:48 +0500

    ADD: Первый атом

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,11 +1,14 @@
 import { Text, View } from 'react-native'
 import { Colors } from '../../shared/tokens'
 import { CustomLink } from '../../shared/CustomLink/CustomLink'
+import { profileAtom } from '../../entities/user/model/user.state'
+import { useAtom } from 'jotai'
 
 export default function AppLayout() {
+    const [profile] = useAtom(profileAtom)
     return (
         <View>
-            <Text style={{ color: Colors.white }}>Мои курсы</Text>
+            <Text style={{ color: Colors.primary }}>{profile.profile?.name} Мои курсы</Text>
             <CustomLink href={'/login'} text={'Логин'} />
             <CustomLink href={'/restore'} text={'Восстановить пароль'} />
             <CustomLink href={'/course/typescript'} text={'Курс ts'} />
