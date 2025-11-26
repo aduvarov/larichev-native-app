849225c65b98996855421289ba2ea6ede1b27837
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-26 12:55:43 +0500

    ADD: Redirect компонент

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from 'expo-router'
+import { Redirect, Stack } from 'expo-router'
+import { useAtomValue } from 'jotai'
+import { authAtom } from '../../entities/auth/model/auth.state'
 
 export default function AppLayout() {
+    const { accessToken } = useAtomValue(authAtom)
+    if (!accessToken) {
+        return <Redirect href={'/login'} />
+    }
     return <Stack></Stack>
 }
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,30 +1,10 @@
 import { Text, View } from 'react-native'
 import { Colors } from '../../shared/tokens'
-import { CustomLink } from '../../shared/CustomLink/CustomLink'
-import { profileAtom } from '../../entities/user/model/user.state'
-import { useAtom, useAtomValue } from 'jotai'
-import { authAtom } from '../../entities/auth/model/auth.state'
-import { useEffect } from 'react'
-import { router, useRootNavigationState } from 'expo-router'
 
 export default function AppLayout() {
-    const [profile] = useAtom(profileAtom)
-    const { accessToken } = useAtomValue(authAtom)
-    const state = useRootNavigationState()
-
-    useEffect(() => {
-        if (!state?.key) return
-        if (!accessToken) {
-            router.replace('/login')
-        }
-    }, [accessToken])
-
     return (
         <View>
-            <Text style={{ color: Colors.primary }}>{profile.profile?.name} Мои курсы</Text>
-            <CustomLink href={'/login'} text={'Логин'} />
-            <CustomLink href={'/restore'} text={'Восстановить пароль'} />
-            <CustomLink href={'/course/typescript'} text={'Курс ts'} />
+            <Text style={{ color: Colors.primary }}> Мои курсы</Text>
         </View>
     )
 }
