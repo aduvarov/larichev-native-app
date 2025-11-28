d2a3294f6f805baf9eb0ae1bcccb781be1320548
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-28 19:36:53 +0500

    ADD: Стилизация Drawer

---
+++ entities/layout/ui/CloseDrawer/CloseDrawer.tsx
@@ -0,0 +1,22 @@
+import { View, Pressable, StyleSheet } from 'react-native'
+import { DrawerContentComponentProps } from '@react-navigation/drawer'
+import CloseIcon from '../../../../assets/icons/CloseIcon'
+
+export const CloseDrawer = ({ navigation }: DrawerContentComponentProps) => {
+    return (
+        <Pressable onPress={() => navigation.closeDrawer()}>
+            <View style={styles.button}>
+                <CloseIcon />
+            </View>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        right: 20,
+    },
+})
--- entities/layout/ui/CustomDrawer/CustomDrawer.tsx
+++ entities/layout/ui/CustomDrawer/CustomDrawer.tsx
@@ -2,18 +2,25 @@ import { DrawerContentComponentProps, DrawerContentScrollView } from '@react-nav
 import { Image, StyleSheet, Text, View } from 'react-native'
 import { Colors } from '../../../../shared/tokens'
 import { CustomLink } from '../../../../shared/CustomLink/CustomLink'
+import { CloseDrawer } from '../CloseDrawer/CloseDrawer'
+import { useSetAtom } from 'jotai'
+import { logoutAtom } from '../../../auth/model/auth.state'
 
 export const CustomDrawer = (props: DrawerContentComponentProps) => {
+    const logout = useSetAtom(logoutAtom)
     return (
         <DrawerContentScrollView {...props} contentContainerStyle={styles.scrollview}>
-            <Text>Drawer</Text>
-            <View>
+            <View style={styles.content}>
+                <Text>Text</Text>
+                <CloseDrawer {...props} />
+            </View>
+            <View style={styles.footer}>
+                <CustomLink text="Выход" href="/login" onPress={() => logout()} />
                 <Image
                     style={styles.logo}
                     source={require('../../../../assets/logo.png')}
                     resizeMode="contain"
                 />
-                <CustomLink text="Выход" href="/login" />
             </View>
         </DrawerContentScrollView>
     )
@@ -24,5 +31,9 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: Colors.black,
     },
-    logo: {},
+    content: { flex: 1 },
+    footer: { gap: 50, marginBottom: 40, alignItems: 'center' },
+    logo: {
+        width: 160,
+    },
 })
