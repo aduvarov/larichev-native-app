2b6f2001a3c6959a96ffba3dd55f1312b1647766
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-01-16 16:01:55 +0500

    ADD: FlatList

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,9 +1,9 @@
-import { ScrollView, StyleSheet, View } from 'react-native'
-import { Gaps } from '../../shared/tokens'
+import { FlatList, StyleSheet, View } from 'react-native'
 import { useAtomValue, useSetAtom } from 'jotai'
 import { courseAtom, loadCourseAtom } from '../../entities/course/model/course.state'
 import { useEffect } from 'react'
 import { CourseCard } from '../../entities/course/ui/CourseCard/CourseCard'
+import { StudentCourseDescription } from '../../entities/course/model/course.model'
 
 export default function AppLayout() {
     // const { isLoading, error, courses } = useAtomValue(courseAtom)
@@ -14,19 +14,29 @@ export default function AppLayout() {
         loadCourse()
     }, [])
 
-    return (
-        <ScrollView>
-            <View style={styles.wrapper}>
-                {courses.length > 0 && courses.map((c) => <CourseCard {...c} key={c.id} />)}
+    const renderCourse = ({ item }: { item: StudentCourseDescription }) => {
+        return (
+            <View style={styles.item}>
+                <CourseCard {...item} />
             </View>
-        </ScrollView>
+        )
+    }
+
+    return (
+        <>
+            {courses.length > 0 && (
+                <FlatList
+                    data={courses}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={renderCourse}
+                />
+            )}
+        </>
     )
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        flexDirection: 'column',
-        gap: Gaps.g20,
+    item: {
         padding: 20,
     },
 })
