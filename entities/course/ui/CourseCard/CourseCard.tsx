50080eb600192a0fd975d48766698d95c1e8c1fa
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-01-16 20:25:36 +0500

    ADD: Градиентный текст

--- entities/course/ui/CourseCard/CourseCard.tsx
+++ entities/course/ui/CourseCard/CourseCard.tsx
@@ -3,12 +3,16 @@ import { Colors, Fonts, Gaps, Radius } from '../../../../shared/tokens'
 import { StudentCourseDescription } from '../../model/course.model'
 import { Button } from '../../../../shared/Button/Button'
 import { Chip } from '../../../../shared/Chip/Chip'
+import MaskedView from '@react-native-masked-view/masked-view'
+import { LinearGradient } from 'expo-linear-gradient'
 
 export const CourseCard = ({
+    id,
     shortTitle,
     image,
     courseOnDirection,
     alias,
+    tariffs,
 }: Partial<StudentCourseDescription>) => {
     return (
         <View style={styles.card}>
@@ -16,9 +20,26 @@ export const CourseCard = ({
             <View style={styles.header}>
                 <Text style={styles.title}>{shortTitle}</Text>
                 <View style={styles.chips}>
-                    {courseOnDirection?.length > 0 &&
-                        courseOnDirection?.map((c) => <Chip text={c.direction.name} />)}
+                    {courseOnDirection?.length &&
+                        courseOnDirection?.map((c) => <Chip text={c.direction.name} key={id} />)}
                 </View>
+                <MaskedView
+                    maskElement={
+                        <Text style={styles.tariff}>
+                            Тариф &laquo;{tariffs?.[0]?.name ?? '-'}&raquo;
+                        </Text>
+                    }
+                >
+                    <LinearGradient
+                        colors={['#D77BE5', '#6C38CC']}
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 1, y: 0 }}
+                    >
+                        <Text style={{ ...styles.tariff, ...styles.tariffWithOpacity }}>
+                            Тариф &laquo;{tariffs?.[0]?.name ?? '-'}&raquo;
+                        </Text>
+                    </LinearGradient>
+                </MaskedView>
             </View>
             <View style={styles.footer}>
                 <Button
@@ -35,6 +56,14 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         borderRadius: Radius.r10,
         backgroundColor: Colors.blackLight,
+    },
+    tariff: {
+        marginTop: 10,
+        fontSize: Fonts.f16,
+        fontFamily: Fonts.regular,
+    },
+    tariffWithOpacity: {
+        opacity: 0,
     },
     image: {
         borderRadius: 10,
